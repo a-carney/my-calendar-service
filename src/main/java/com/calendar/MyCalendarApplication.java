d1e038c9622097291ee802a31764c4b790c751a0
Alex Carney <alexwcarney@gmail.com>
2025-05-12 20:36:35 -0400

    creates pom, properties and main class for main and test

---
+++ src/main/java/com/calendar/MyCalendarApplication.java
@@ -0,0 +1,12 @@
+package com.calendar;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCalendarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyCalendarApplication.class, args);
+    }
+}
---
+++ src/test/java/com/calendar/MyCalendarApplicationTest.java
@@ -0,0 +1,16 @@
+package com.calendar;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MyCalendarApplicationTest
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
